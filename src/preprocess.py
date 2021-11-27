0c5949620eb0e5918f1fdec5daad1f02eddc9072
cxh0210 <cxhanthea@126.com>
2021-11-27 13:14:25 -0800

    Add BOG for dataset

--- src/preprocess.py
+++ src/preprocess.py
@@ -17,6 +17,7 @@ from docopt import docopt
 import pandas as pd
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import CountVectorizer
 import os
 
 opt = docopt(__doc__)
@@ -28,28 +29,36 @@ def main(train_path, test_path, out_file_train, out_file_test):
 
     # Relevant transform
     categorical_features = ["Brand", "Style", "Country"]
-    drop_features = ["Review","Variety", "Top Ten"]
+    text_feature = "Variety"
+
+    drop_features = ["Review","Top Ten"]
     target = ["Stars"]
 
     categorical_transformer = OneHotEncoder(handle_unknown="ignore",sparse=False)
+    text_transformer = CountVectorizer(stop_words="english")
 
     preprocessor = make_column_transformer(
-     (categorical_transformer, categorical_features)
+     (categorical_transformer, categorical_features),
+     (text_transformer, text_feature)
     )
+
     train_trans = preprocessor.fit_transform(df_train)
     test_trans = preprocessor.transform(df_test)
 
-    # Get the transformed data 
+    # Get the transformed data
+    ohe_col = preprocessor.named_transformers_["onehotencoder"].get_feature_names_out().tolist()
+    text_col = preprocessor.named_transformers_["countvectorizer"].get_feature_names_out().tolist()
+
     train_out = pd.DataFrame(
-      data=train_trans,
-      columns=preprocessor.get_feature_names_out(),
+      data=train_trans.toarray(),
+      columns=ohe_col+text_col,
       index=df_train.index,
     )
     train_out["Stars"] = df_train["Stars"]
 
     test_out = pd.DataFrame(
-      data=test_trans,
-      columns=preprocessor.get_feature_names_out(),
+      data=test_trans.toarray(),
+      columns=ohe_col+text_col,
       index=df_test.index,
     )
     test_out["Stars"] = df_test["Stars"]
