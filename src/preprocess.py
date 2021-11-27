02c55387f3ead765305e081e1f76658e2c0f67d6
cxh0210 <cxhanthea@126.com>
2021-11-27 12:20:37 -0800

    Avoid fit twice for train and test df

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,11 +3,13 @@
 
 """preprocess for dataframe
 
-Usage: preprocess.py --path=<path> --out_file=<out_file> 
+Usage: preprocess.py --train_path=<train_path> --test_path=<test_path> --out_file_train=<out_file_train> --out_file_test=<out_file_test>  
  
 Options:
---path=<path>             the data to process
---out_file=<out_file>   Path (including filename) of where to locally write the file
+--train_path=<train_path>             the train dataframe to process
+--test_path=<test_path>               the test dataframe to process
+--out_file_train=<out_file_train>   Path (including filename) of where to locally write the train data
+--out_file_test=<out_file_test>     Path (including filename) of where to locally write the test data
 """
 
 import pycountry
@@ -19,13 +21,14 @@ import os
 
 opt = docopt(__doc__)
 
-def main(path, out_file):
-    # Read the df
-    df = pd.read_csv(path)
+def main(train_path, test_path, out_file_train, out_file_test):
+    # Read two df
+    df_train = pd.read_csv(train_path)
+    df_test = pd.read_csv(test_path)
 
     # Relevant transform
-    categorical_features = ["Brand", "Variety", "Style", "Country"]
-    drop_features = ["Review","Top Ten"]
+    categorical_features = ["Brand", "Style", "Country"]
+    drop_features = ["Review","Variety", "Top Ten"]
     target = ["Stars"]
 
     categorical_transformer = OneHotEncoder(handle_unknown="ignore",sparse=False)
@@ -33,21 +36,37 @@ def main(path, out_file):
     preprocessor = make_column_transformer(
      (categorical_transformer, categorical_features)
     )
-    df_trans = preprocessor.fit_transform(df)
+    train_trans = preprocessor.fit_transform(df_train)
+    test_trans = preprocessor.transform(df_test)
 
     # Get the transformed data 
-    df_out = pd.DataFrame(
-      data=df_trans,
+    train_out = pd.DataFrame(
+      data=train_trans,
       columns=preprocessor.get_feature_names_out(),
-        index=df.index,
+      index=df_train.index,
     )
-    df_out["Stars"] = df["Stars"]
+    train_out["Stars"] = df_train["Stars"]
 
-    # Save the data as csv
+    test_out = pd.DataFrame(
+      data=test_trans,
+      columns=preprocessor.get_feature_names_out(),
+      index=df_test.index,
+    )
+    test_out["Stars"] = df_test["Stars"]
+
+    # Save the train data as csv
     try:
-        df_out.to_csv(out_file, index = False)
+        train_out.to_csv(out_file_train, index = False)
     except:
-        os.makedirs(os.path.dirname(out_file))
-        df_out.to_csv(out_file, index = False)
+        os.makedirs(os.path.dirname(out_file_train))
+        train_out.to_csv(out_file_train, index = False)
+
+    # Save the test data as csv
+    try:
+        test_out.to_csv(out_file_test, index = False)
+    except:
+        os.makedirs(os.path.dirname(out_file_test))
+        test_out.to_csv(out_file_test, index = False)
+
 if __name__ == "__main__":
-    main(opt["--path"], opt["--out_file"])
+    main(opt["--train_path"], opt["--test_path"], opt["--out_file_train"], opt["--out_file_test"])
